d46306330b25422d0b06fdf9f3f9a2bc84bdcf6b
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 20:02:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,9 +3,14 @@ class BankAccount
   
   def initialize(owner_name)
     @name = owner_name
+    @balance = 1000
   end
   
   def name
     @name
+  end
+  
+  def balance
+    @balance
   end
 end
