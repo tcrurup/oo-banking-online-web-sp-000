16991762913eca016b5da237e95c008d5abf32d2
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 20:11:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,21 @@
 class Transfer
   
   
-  def initialize
-    
+  def initialize(origin_account, designated_account, transfer_amount)
+    @origin = origin_account
+    @designation = designated_account
+    @amount = transfer_amount
+  end
+  
+  def origin
+    @origin
+  end
+  
+  def destination
+    @destination
+  end
+  
+  def amount
+    @amount
   end
 end
